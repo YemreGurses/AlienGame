c887710e5de5558cebf9bd8d36b87222393141ce
furkan <furkandoganktf@gmail.com>
2018-11-14 16:57:24 +0300

    CENG 453 Some little changes at service and controller

--- src/main/java/ceng453/controller/UserController.java
+++ src/main/java/ceng453/controller/UserController.java
@@ -1,22 +1,18 @@
 package ceng453.controller;
 
 
-import javax.validation.Valid;
-
-
 import ceng453.entity.User;
 import ceng453.repository.UserRepository;
 import ceng453.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -45,12 +41,18 @@ public class UserController {
 //        return modelAndView;
 //    }
 
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public List<User> getUsers() {
+    @GetMapping(value = "/users")
+    public List<User> getAllUser() {
         List<User> userList = userRepository.findAll();
         return userList;
     }
 
+
+    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
+    public Optional<User> getUser(@PathVariable Integer userId) {
+        Optional<User> user = userRepository.findById(userId);
+        return user;
+    }
 //
 //    @RequestMapping(value = "/registration", method = RequestMethod.POST)
 //    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
