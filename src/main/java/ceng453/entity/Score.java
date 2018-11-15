a9db624a6c8a2da10f192fe680765804ec4c1e9d
furkan <furkandoganktf@gmail.com>
2018-11-15 13:26:26 +0300

    CENG 453 User Controller Test changed

--- src/main/java/ceng453/entity/Score.java
+++ src/main/java/ceng453/entity/Score.java
@@ -1,12 +1,18 @@
 package ceng453.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "score")
 public class Score {
--- src/test/java/ceng453/UserControllerTest.java
+++ src/test/java/ceng453/UserControllerTest.java
@@ -3,6 +3,7 @@ package ceng453;
 
 import ceng453.entity.Score;
 import ceng453.entity.User;
+import ceng453.repository.ScoreRepository;
 import ceng453.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
@@ -24,6 +22,9 @@ public class UserControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ScoreRepository scoreRepository;
 
     @Test
     public void findAll() {
@@ -54,21 +55,41 @@ public class UserControllerTest {
         assertEquals(yemre,userRepository.findById(2).get());
     }
 
-//    @Test
-//    public void getLeaderBoard() {
-//
-////        List<Map<String, String>> listScore = Arrays.asList(new Map<String, String>[]{(1, 100), (2, 500)} );
-//
-//
-//        User furkan = User.builder().name("furkan1").email("furkan@a").password("asdfg").scoreList().build();
-//        userRepository.save(furkan);
-//        User yemre = User.builder().name("yemre1").email("yemre@a").password("asdfg").build();
-//        userRepository.save(yemre);
-//
-//
-//        assertNotNull(userRepository.findById(2));
-//
-//        assertEquals(yemre,userRepository.findById(2));
-//    }
+    @Test
+    public void getLeaderBoard() {
+
+
+        Date today = new Date();
+        Score score1 = Score.builder().score(10).date(today).build();
+        Score score2 = Score.builder().score(20).date(today).build();
+        Score score3 = Score.builder().score(30).date(today).build();
+        Score score4 = Score.builder().score(40).date(today).build();
+
+        scoreRepository.save(score1);
+        scoreRepository.save(score2);
+        scoreRepository.save(score3);
+        scoreRepository.save(score4);
+
+        List<Score> scoreList1 = new ArrayList<>();
+        List<Score> scoreList2 = new ArrayList<>();
+        scoreList1.add(score1);
+        scoreList1.add(score2);
+        scoreList2.add(score3);
+        scoreList2.add(score4);
+
+        User furkan = User.builder().name("furkan1").email("furkan@a").password("asdfg").scoreList(scoreList1).build();
+        userRepository.save(furkan);
+        User yemre = User.builder().name("yemre1").email("yemre@a").password("asdfg").scoreList(scoreList2).build();
+        userRepository.save(yemre);
+
+        assertEquals("70",scoreRepository.getLeaderBoard().get(0).values().toArray()[0]);
+
+        assertEquals("yemre1",scoreRepository.getLeaderBoard().get(0).values().toArray()[1]);
+
+        assertEquals("30",scoreRepository.getLeaderBoard().get(1).values().toArray()[0]);
+
+        assertEquals("furkan1",scoreRepository.getLeaderBoard().get(1).values().toArray()[1]);
+
+    }
 
 }
