b33beec55e0de32ba22783dd46a55413098da01f
furkan <furkandoganktf@gmail.com>
2018-12-13 01:00:30 +0300

    Change File Order and add Front-End Class

--- Back-End/src/main/java/ceng453/AllienGameApplication.java
+++ Back-End/src/main/java/ceng453/AllienGameApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AllienGameApplication {
 
-    public static void main(String[] args) throws Throwable{
+    public static void main(String[] args) {
         SpringApplication.run(AllienGameApplication.class, args);
     }
 }
